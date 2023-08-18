0d4934e8451316ec8223d16d333fddf1015a4c53
Bellhior <cliff_burton85@mail.ru>
2023-08-18 09:40:19 +0400

    homework2_done

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -18,3 +18,13 @@ def test_push(stack):
     assert stack.top.next_node.data == "data2"
     assert stack.top.next_node.next_node.data == "data1"
     assert stack.top.next_node.next_node.next_node is None
+
+
+# TestCase3
+def test_pop(stack):
+    assert stack.pop() == "data3"
+    assert stack.top.data == "data2"
+    assert stack.pop() == "data2"
+    assert stack.top.data == "data1"
+    assert stack.pop() == "data1"
+    assert stack.top is None
